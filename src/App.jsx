5e7752a7f8398086bf611009f90316e45a01ff83
baranideveloper <baranikumarkgp98@gmail.com>
2024-02-02 11:11:03 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-
 import './App.css'
 import './style.css'
 import Career from './pages/Career'
